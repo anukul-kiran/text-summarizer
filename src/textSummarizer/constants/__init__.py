18f26be805fb3fc0c6510355290cee4407c23164
anukul-kiran <balasubramanian120@gmail.com>
2024-04-20 22:01:19 +0530

    Data Ingestion

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path('C:/Users/balas/Documents/projects/text-summarizer/config/config.yaml')
+PARAMS_FILE_PATH = Path('C:/Users/balas/Documents/projects/text-summarizer/params.yaml')
+
